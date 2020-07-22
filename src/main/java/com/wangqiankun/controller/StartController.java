87405546dd7616dfb008c766050ae893843b8741
wangqiankun <877463053@qq.com>
2020-07-22 15:43:10 +0800

    修改了startController

--- src/main/java/com/wangqiankun/controller/StartController.java
+++ src/main/java/com/wangqiankun/controller/StartController.java
@@ -3,7 +3,7 @@ import com.wangqiankun.domian.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+//
 @Controller
 public class StartController {
 
